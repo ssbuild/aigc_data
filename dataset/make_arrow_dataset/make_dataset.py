fb267a8de799441a445822c4864fcea902895567
ssbuild <462304@qq.com>
2023-07-07 00:57:22 +0800

    简化
    Signed-off-by: ssbuild <462304@qq.com>

--- dataset/make_arrow_dataset/make_dataset.py
+++ dataset/make_arrow_dataset/make_dataset.py
@@ -11,63 +11,64 @@ from fastdatasets import gfile
 
 
 
-with_stream = False
-def test_write(in_files,outfile):
-    #获取json文件数据
-    all_data = []
-    for file in in_files:
-        with open(file,mode='r',encoding='utf-8') as f:
-            if os.path.basename(file).find('未完成') != -1:
-                continue
+with_stream = True
+schema = {
+    'id': 'int32',
+    'instruction': 'str',
+    'input': 'str',
+    'output': 'str',
+    'file': 'str',
+}
 
-            jds = json.loads(f.read())
-            all_data.append((os.path.basename(file),jds))
-    #构建schema
-    schema = {
-        'id': 'int32',
-        'instruction': 'str',
-        'input': 'str',
-        'output': 'str',
-        'file': 'str',
-    }
-    #创建文件
-    fs = PythonWriter(outfile,schema=schema,with_stream=with_stream,options=None)
 
-    N = 1000
-    for file, jds in all_data:
-        batch = {k: [] for k in schema}
-        for i, jd in enumerate(jds):
-            batch["id"].append(i)
-            batch["file"].append(file)
-
-            for k,v in jd.items():
-                # 在本数据集中，为无效字段 脏数据，直接扔掉
-                if k == 'history':
+class DataWriter:
+    def get_file_data(self,in_files):
+        all_data = []
+        for file in in_files:
+            with open(file, mode='r', encoding='utf-8') as f:
+                if os.path.basename(file).find('未完成') != -1:
                     continue
-                batch[k].append(v)
-            # 在本数据集中，input 可能不存在
-            if 'input' not in jd:
-                batch['input'].append('')
-            #数据积攒到N ，同步磁盘文件
-            if len(batch["id"]) % N == 0:
+                jds = json.loads(f.read())
+                all_data.append((os.path.basename(file), jds))
+        return all_data
+
+    def write(self,in_files,outfile):
+        all_data = self.get_file_data(in_files)
+        fs = PythonWriter(outfile,schema=schema,with_stream=with_stream,options=None)
+        N = 1000
+        for file, jds in all_data:
+            batch = {k: [] for k in schema}
+            for i, jd in enumerate(jds):
+                batch["id"].append(i)
+                batch["file"].append(file)
+
+                for k, v in jd.items():
+                    if k == 'history':
+                        continue
+                    batch[k].append(v)
+                if 'input' not in jd:
+                    batch['input'].append('')
+
+                if len(batch["id"]) % N == 0:
+                    status = fs.write_batch(batch.keys(), batch.values())
+                    assert status.ok(), status.message()
+                    for k, v in batch.items():
+                        v.clear()
+            if len(batch["id"]):
                 status = fs.write_batch(batch.keys(), batch.values())
                 assert status.ok(), status.message()
-                for k,v in batch.items():
+                for k, v in batch.items():
                     v.clear()
+        fs.close()
 
-        if len(batch["id"]):
-            status = fs.write_batch(batch.keys(), batch.values())
-            assert status.ok(), status.message()
-            for k, v in batch.items():
-                v.clear()
-    fs.close()
+    @staticmethod
+    def read(file):
+        dataset = load_dataset.RandomDataset(file,with_share_memory=not with_stream)
+        print(file,'total', len(dataset))
+        for i in range(len(dataset)):
+            print(dataset[i])
+            break
 
-def test_random(file):
-    dataset = load_dataset.RandomDataset(file,with_share_memory=not with_stream)
-    print(file,'total', len(dataset))
-    for i in range(len(dataset)):
-        print(dataset[i])
-        break
 
 
 
@@ -80,5 +81,5 @@ if __name__ == '__main__':
         (gfile.glob(os.path.join(base_dir,'其他中文问题补充/*.json')), os.path.join(base_dir,'other_chinese.arrow')),
     ]
     for files,outfile in in_files:
-        test_write(files,outfile)
-        test_random(outfile)
+        DataWriter().write(files,outfile)
+        DataWriter.read(outfile)
--- dataset/make_parquet_dataset/make_dataset.py
+++ dataset/make_parquet_dataset/make_dataset.py
@@ -15,59 +15,62 @@ from fastdatasets.parquet.writer import PythonWriter
 from fastdatasets.parquet.dataset import load_dataset,arrow
 from fastdatasets import gfile
 
-
-
 with_stream = True
-def test_write(in_files,outfile):
-    all_data = []
-    for file in in_files:
-        with open(file,mode='r',encoding='utf-8') as f:
-            if os.path.basename(file).find('未完成') != -1:
-                continue
+schema = {
+    'id': 'int32',
+    'instruction': 'str',
+    'input': 'str',
+    'output': 'str',
+    'file': 'str',
+}
 
-            jds = json.loads(f.read())
-            all_data.append((os.path.basename(file),jds))
-
-    schema = {'id': 'int32',
-              'instruction': 'str',
-              'input': 'str',
-              'output': 'str',
-              'file': 'str',
-              }
-    fs = PythonWriter(outfile,schema=schema)
-
-    N = 1000
-    for file, jds in all_data:
-        batch = {k: [] for k in schema}
-        for i, jd in enumerate(jds):
-            batch["id"].append(i)
-            batch["file"].append(file)
-
-            for k,v in jd.items():
-                if k == 'history':
+class DataWriter:
+    def get_file_data(self,in_files):
+        all_data = []
+        for file in in_files:
+            with open(file, mode='r', encoding='utf-8') as f:
+                if os.path.basename(file).find('未完成') != -1:
                     continue
-                batch[k].append(v)
-            if 'input' not in jd:
-                batch['input'].append('')
+                jds = json.loads(f.read())
+                all_data.append((os.path.basename(file), jds))
+        return all_data
 
-            if len(batch["id"]) % N == 0:
+    def write(self,in_files,outfile):
+        all_data = self.get_file_data(in_files)
+        fs = PythonWriter(outfile, schema=schema)
+        N = 1000
+        for file, jds in all_data:
+            batch = {k: [] for k in schema}
+            for i, jd in enumerate(jds):
+                batch["id"].append(i)
+                batch["file"].append(file)
+
+                for k, v in jd.items():
+                    if k == 'history':
+                        continue
+                    batch[k].append(v)
+                if 'input' not in jd:
+                    batch['input'].append('')
+
+                if len(batch["id"]) % N == 0:
+                    status = fs.write_batch(batch.keys(), batch.values())
+                    assert status.ok(), status.message()
+                    for k, v in batch.items():
+                        v.clear()
+            if len(batch["id"]):
                 status = fs.write_batch(batch.keys(), batch.values())
-                assert status.ok(),status.message()
-                for k,v in batch.items():
+                assert status.ok(), status.message()
+                for k, v in batch.items():
                     v.clear()
-        if len(batch["id"]):
-            status = fs.write_batch(batch.keys(), batch.values())
-            assert status.ok(), status.message()
-            for k, v in batch.items():
-                v.clear()
-    fs.close()
+        fs.close()
 
-def test_random(file):
-    dataset = load_dataset.RandomDataset(file,with_share_memory=not with_stream)
-    print(file,'total', len(dataset))
-    for i in range(len(dataset)):
-        print(dataset[i])
-        break
+    @staticmethod
+    def read(file):
+        dataset = load_dataset.RandomDataset(file,with_share_memory=not with_stream)
+        print(file,'total', len(dataset))
+        for i in range(len(dataset)):
+            print(dataset[i])
+            break
 
 
 
@@ -79,5 +82,5 @@ if __name__ == '__main__':
         (gfile.glob(os.path.join(base_dir, '其他中文问题补充/*.json')), os.path.join(base_dir, 'other_chinese.parquet')),
     ]
     for files, outfile in in_files:
-        test_write(files, outfile)
-        test_random(outfile)
+        DataWriter().write(files, outfile)
+        DataWriter.read(outfile)
