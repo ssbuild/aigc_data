c79d68faf4fbbcdae3a1fa0dee485f8e412cebb3
ssbuild <462304@qq.cn>
2023-10-23 16:22:03 +0800

    训练集评估
    Signed-off-by: ssbuild <462304@qq.cn>

--- clean_script/law/jecqa/process_state3.py
+++ clean_script/law/jecqa/process_state3.py
@@ -1,54 +1,72 @@
 # -*- coding: utf-8 -*-
 # @Author  : ssbuild
 # @Time    : 2023/10/17 11:10
-import os.path
-
+import os
+import random
 import pandas as pd
 import glob
 
+random.seed(42)
 
-fs_list = glob.glob(r'F:\nlpdata_2023\JEC-QA\clean\dev\*.csv')
-os.makedirs(r"F:\nlpdata_2023\JEC-QA\clean\dev_single",exist_ok=True)
-os.makedirs(r"F:\nlpdata_2023\JEC-QA\clean\dev_muti",exist_ok=True)
+def make_evals(fs_list,output_dir,shuffle=False,limit_single=-1,limit_muti=-1):
+    os.makedirs(os.path.join(output_dir, "single"),exist_ok=True)
+    os.makedirs(os.path.join(output_dir, "muti"), exist_ok=True)
+    for filename in fs_list:
+        df = pd.read_csv(filename)
+
+        singles,mutis = [],[]
+        n1,n2 = 0,0
+        idx_ = 0
+        for idx, question, A, B, C, D, answer in zip(df["id"], df["question"], df["A"], df["B"], df["C"], df["D"],
+                                                     df["answer"]):
+            answer = eval(answer)
+            if len(answer) == 1:
+                n1 += 1
+                n = n1
+                result = singles
+            else:
+                n2 += 1
+                n = n2
+                result = mutis
+
+            idx_ += 1
+
+            result.append({
+                "id": n,
+                "question": question,
+                "A": A,
+                "B": B,
+                "C": C,
+                "D": D,
+                "answer": answer,
+            })
+
+        if shuffle:
+            random.shuffle(singles)
+            random.shuffle(mutis)
+        if limit_single > 0:
+            singles = singles[:limit_single]
+
+        if limit_muti > 0:
+            mutis = mutis[:limit_muti]
+
+        df_single = pd.DataFrame(singles)
+        df_muti = pd.DataFrame(mutis)
 
 
-# 将评估dev数据集 , 制作成 aigc_evals 评估格式
-
-for filename in fs_list:
-    df = pd.read_csv(filename)
-
-    singles,mutis = [],[]
-    n1,n2 = 0,0
-    idx_ = 0
-    for idx, question, A, B, C, D, answer in zip(df["id"], df["question"], df["A"], df["B"], df["C"], df["D"],
-                                                 df["answer"]):
-        answer = eval(answer)
-        if len(answer) == 1:
-            n1 += 1
-            n = n1
-            result = singles
-        else:
-            n2 += 1
-            n = n2
-            result = mutis
-
-        idx_ += 1
-
-        result.append({
-            "id": n,
-            "question": question,
-            "A": A,
-            "B": B,
-            "C": C,
-            "D": D,
-            "answer": answer,
-        })
+        basename = os.path.basename(filename)
+        df_single.to_csv(os.path.join(output_dir,"single",basename),index=False)
+        df_muti.to_csv(os.path.join(output_dir, "muti", basename),index=False)
 
 
-    df_single = pd.DataFrame(singles)
-    df_muti = pd.DataFrame(mutis)
+if __name__ == '__main__':
+    # 将评估dev数据集 , 制作成 aigc_evals 评估格式
+    fs_list = glob.glob(r'F:\nlpdata_2023\JEC-QA\clean\dev\*.csv')
+    make_evals(fs_list,
+               r'F:\nlpdata_2023\JEC-QA\clean\evals\dev')
 
-    dirname = os.path.join(os.path.dirname(filename),'..')
-    basename = os.path.basename(filename)
-    df_single.to_csv(os.path.join(dirname,"dev_single",basename),index=False)
-    df_muti.to_csv(os.path.join(dirname, "dev_muti", basename),index=False)
+    # 将评估train数据集 , 制作成 aigc_evals 评估格式
+    fs_list = glob.glob(r'F:\nlpdata_2023\JEC-QA\clean\train\*.csv')
+    make_evals(fs_list,
+               r'F:\nlpdata_2023\JEC-QA\clean\evals\train',
+               shuffle=True,limit_single=400,limit_muti=400)
